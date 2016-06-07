401c86f013e4c666a00e22d9280088fcd47066be
Ubuntu <ubuntu@ip-172-31-30-254.us-west-2.compute.internal>
2016-06-07 13:53:05 +0000

    add rest api

---
+++ rest.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("X-Content-Type-Option: nosniff");
+$pdo = new PDO('sqlite:/tmp/test.db');
+switch($_SERVER['REQUEST_METHOD']){
+  case 'GET':
+    $st = $pdo->query("SELECT * FROM sample");
+    echo json_encode($st->fetchAll(PDO::FETCH_ASSOC));
+    break;
+  case 'POST':
+    $in = json_decode(file_get_contents('php://input'), true);
+    $st = $pdo->prepare("INSERT INTO sample(key,value) VALUES(:key,:value)");
+    $st->execute($in);
+    break;
+  case 'DELETE':
+    $st = $pdo->prepare("DELETE FROM sample WHERE key=?");
+    $st->execute([$_GET['key']]);
+    break;
+}
+
