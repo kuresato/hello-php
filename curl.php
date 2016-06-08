74eb3db47848fbbfc2ebc09b857ac028535ea9e7
admin <admin@ose-other01.localhost.localdomain>
2016-06-08 20:59:44 +0900

    change port

--- curl.php
+++ curl.php
@@ -3,7 +3,7 @@
 <pre>
 <?php
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, 'http://192.168.1.204/rest.php');
+curl_setopt($curl, CURLOPT_URL, 'http://192.168.1.204:8080/rest.php');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $res = curl_exec($curl);
 $header = curl_getinfo($curl);
