3116ab13ec44e1abe9862341948687ea00451629
kuresato <kuresato@gmail.com>
2016-08-04 14:34:12 +0900

    test

--- drbd/index.php
+++ drbd/index.php
@@ -1,3 +1,15 @@
 <?php
-echo 'Hello!';
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+header("X-Content-Type-Option: nosniff");
+
+//$pdo = new PDO('sqlite:/tmp/test.db');
+//$pdo = new PDO('mysql:dbname=sampledb;host='.getenv('MYSQL_SERVICE_HOST'), 'user1', 'user1');
+
+for ($i = 0; $i < 10; $i++) {
+    $sql = "INSERT INTO sample(f1, f2) VALUES(?, ?)";
+    print  "<p>$sql</p>";
+    //$st = $pdo->prepare($sql);
+    //$st->execute(i, i);
+}
 ?>
