1c2a541977b9c244b8e7fe29a8f970df0aaddc23
kuresato <kuresato@gmail.com>
2016-08-04 15:51:08 +0900

    create test code.

--- drbd/index.php
+++ drbd/index.php
@@ -1,15 +1,30 @@
 <?php
-<?php
 header("Content-Type: application/json; charset=UTF-8");
 header("X-Content-Type-Option: nosniff");
 
-//$pdo = new PDO('sqlite:/tmp/test.db');
-//$pdo = new PDO('mysql:dbname=sampledb;host='.getenv('MYSQL_SERVICE_HOST'), 'user1', 'user1');
+$DB_HOST=getenv('MYSQL_SERVICE_HOST');
+$DB_USER='user1';
+$DB_PASS='user1';
 
-for ($i = 0; $i < 10; $i++) {
-    $sql = "INSERT INTO sample(f1, f2) VALUES(?, ?)";
-    print  "<p>$sql</p>";
-    //$st = $pdo->prepare($sql);
-    //$st->execute(i, i);
+print "<html><body>\n";
+print "<div>$DB_HOST</div>\n";
+
+$file = fopen("/data/test.txt", "a+");
+flock($file, LOCK_EX);
+
+$pdo = new PDO('mysql:dbname=sampledb;host='.$DB_HOST, $DB_USER, $DB_PASS);
+
+for ($i = 0; $i < 300; $i++) {
+    print  "$i\n";
+    fputs($file, "$i\n");
+
+    $sql = "INSERT INTO sample(f1, f2) VALUES(:F1, :F2)";
+    $st = $pdo->prepare($sql);
+    $st->execute(array(':F1'=>$i, ':F2'=>rand()));
+    sleep(1);
 }
+
+flock($file, LOCK_UN);
+
+print "</body></html>\n";
 ?>
