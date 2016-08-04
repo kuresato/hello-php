156cf1a5a65f37ef28b1785190840a18abe9d30a
kuresato <kuresato@gmail.com>
2016-08-04 18:11:09 +0900

    modify

--- drbd/index.php
+++ drbd/index.php
@@ -11,6 +11,7 @@ print "<div>$DB_HOST</div>\n";
 
 $file = @fopen("/data/test.txt", "a+") || die('file open error.');
 print "<p>file: $file</p>\n";
+if(!$file) die ("file is false\n");
 //flock($file, LOCK_EX);
 
 $pdo = new PDO('mysql:dbname=sampledb;host='.$DB_HOST, $DB_USER, $DB_PASS);
@@ -22,6 +23,8 @@ for ($i = 0; $i < 300; $i++) {
     $sql = "INSERT INTO sample(f1, f2) VALUES(:F1, :F2)";
     $st = $pdo->prepare($sql);
     $st->execute(array(':F1'=>$i, ':F2'=>rand()));
+
+    flush();
     sleep(1);
 }
 
