09e65437ec53edd8efe8dcfa20a5d05569b230fa
kuresato <kuresato@gmail.com>
2016-12-10 15:56:02 +0900

    Update log1.php

--- log1.php
+++ log1.php
@@ -1,5 +1,16 @@
-<?php                                                                           
+<html>
+  <body>
+
+    <form method="GET" action="log1.php">
+      <input type="text" name="mes" size="40">
+    </form>
+    
+    <pre style="background-color: #EEE">
+<?php
 echo "Logging.";                                                               
 $stdout = fopen('php://stdout', 'w');                                           
-fputs($stdout, "this is debug message\n");                                      
+fputs($stdout, "this is debug message $_GET['mes']);                                      
 ?>
+    </pre>
+  </body>
+</html>
